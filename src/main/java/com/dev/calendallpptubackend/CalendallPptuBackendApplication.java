408d54aa9006c3e3a192ce003ce696f82eeb08a5
barbaragrinop <barbarapereira123@hotmail.com>
2024-05-13 20:00:27 -0300

    feat: inital configuration

---
+++ src/main/java/com/dev/calendallpptubackend/CalendallPptuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dev.calendallpptubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendallPptuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendallPptuBackendApplication.class, args);
+	}
+
+}
